1cf0b183e7f0643405b07433afffced255e81f89
Boody <bodebi639@gmail.com>
2026-06-25 21:38:08 +0300

    added register incrementation instructions and z & c flag modification

--- main.c
+++ main.c
@@ -26,6 +26,10 @@
 #define OP_TXA 0x8A
 #define OP_TSX 0xBA
 #define OP_TXS 0x9A
+#define OP_INX 0xE8
+#define OP_INY 0xC8
+#define OP_DEX 0xCA
+#define OP_DEY 0x88
 
 uint8_t memory[65536];
 
@@ -40,10 +44,22 @@ typedef struct
     uint8_t P;
 } CPU;
 
+// Helper functions
+void set_flag(CPU *cpu, uint8_t flag, uint8_t value)
+{
+    if (value)
+        cpu->P |= flag;
+    else
+        cpu->P &= ~flag;
+}
+
 // Instruction set
 void lda_immediate(CPU *cpu)
 {
     cpu->A = memory[cpu->PC++];
+
+    set_flag(cpu, FLAG_Z, cpu->A == 0);
+    set_flag(cpu, FLAG_N, cpu->A & 0x80);
 }
 
 void lda_absolute(CPU *cpu)
@@ -52,11 +68,17 @@ void lda_absolute(CPU *cpu)
 
     cpu->A = memory[addr];
     cpu->PC += 2;
+
+    set_flag(cpu, FLAG_Z, cpu->A == 0);
+    set_flag(cpu, FLAG_N, cpu->A & 0x80);
 }
 
 void ldx_immediate(CPU *cpu)
 {
     cpu->X = memory[cpu->PC++];
+
+    set_flag(cpu, FLAG_Z, cpu->X == 0);
+    set_flag(cpu, FLAG_N, cpu->X & 0x80);
 }
 
 void ldx_absolute(CPU *cpu)
@@ -65,11 +87,17 @@ void ldx_absolute(CPU *cpu)
 
     cpu->X = memory[addr];
     cpu->PC += 2;
+
+    set_flag(cpu, FLAG_Z, cpu->X == 0);
+    set_flag(cpu, FLAG_N, cpu->X & 0x80);
 }
 
 void ldy_immediate(CPU *cpu)
 {
     cpu->Y = memory[cpu->PC++];
+
+    set_flag(cpu, FLAG_Z, cpu->Y == 0);
+    set_flag(cpu, FLAG_N, cpu->Y & 0x80);
 }
 
 void ldy_absolute(CPU *cpu)
@@ -78,36 +106,86 @@ void ldy_absolute(CPU *cpu)
 
     cpu->Y = memory[addr];
     cpu->PC += 2;
+
+    set_flag(cpu, FLAG_Z, cpu->Y == 0);
+    set_flag(cpu, FLAG_N, cpu->Y & 0x80);
 }
 
 void tax(CPU *cpu)
 {
     cpu->X = cpu->A;
+
+    set_flag(cpu, FLAG_Z, cpu->X == 0);
+    set_flag(cpu, FLAG_N, cpu->X & 0x80);
 }
 
 void tay(CPU *cpu)
 {
     cpu->Y = cpu->A;
+
+    set_flag(cpu, FLAG_Z, cpu->Y == 0);
+    set_flag(cpu, FLAG_N, cpu->Y & 0x80);
 }
 
 void tya(CPU *cpu)
 {
     cpu->A = cpu->Y;
+
+    set_flag(cpu, FLAG_Z, cpu->A == 0);
+    set_flag(cpu, FLAG_N, cpu->A & 0x80);
 }
 
 void txa(CPU *cpu)
 {
     cpu->A = cpu->X;
+
+    set_flag(cpu, FLAG_Z, cpu->A == 0);
+    set_flag(cpu, FLAG_N, cpu->A & 0x80);
 }
 
 void tsx(CPU *cpu)
 {
     cpu->X = cpu->SP;
+
+    set_flag(cpu, FLAG_Z, cpu->X == 0);
+    set_flag(cpu, FLAG_N, cpu->X & 0x80);
 }
 
-void txs(CPU *cpu)
+void txs(CPU *cpu) // TXS Does not affect any flags
 {
     cpu->SP = cpu->X;
+}
+
+void inx(CPU *cpu)
+{
+    cpu->X++;
+
+    set_flag(cpu, FLAG_Z, cpu->X == 0);
+    set_flag(cpu, FLAG_N, cpu->X & 0x80);
+}
+
+void iny(CPU *cpu)
+{
+    cpu->Y++;
+
+    set_flag(cpu, FLAG_Z, cpu->Y == 0);
+    set_flag(cpu, FLAG_N, cpu->Y & 0x80);
+}
+
+void dex(CPU *cpu)
+{
+    cpu->X--;
+
+    set_flag(cpu, FLAG_Z, cpu->X == 0);
+    set_flag(cpu, FLAG_N, cpu->X & 0x80);
+}
+
+void dey(CPU *cpu)
+{
+    cpu->Y--;
+
+    set_flag(cpu, FLAG_Z, cpu->Y == 0);
+    set_flag(cpu, FLAG_N, cpu->Y & 0x80);
 }
 
 int main()
@@ -115,6 +193,8 @@ int main()
     CPU cpu6502;
     memset(&cpu6502, 0, sizeof(cpu6502)); // Initialize CPU registers 0
     memset(&memory, 0, sizeof(memory));   // Initialize memory 0
+
+    memory[0x1000] = 0x50; // value passed to instruction
 
     memory[0] = 0xAC; // instruction LDY_ABS opcode
 
@@ -124,7 +204,7 @@ int main()
     memory[3] = 0xA9; // instruction LDA_IMM opcode
     memory[4] = 0x10; // value passed to instruction
 
-    memory[0x1000] = 0x50; // value passed to instruction
+    memory[6] = 0xC8; // increment y
 
     memory[5] = 0xAA; // instruction TAX opcode
 
@@ -171,6 +251,18 @@ int main()
             break;
         case OP_TXS:
             txs(&cpu6502);
+            break;
+        case OP_INX:
+            inx(&cpu6502);
+            break;
+        case OP_INY:
+            iny(&cpu6502);
+            break;
+        case OP_DEX:
+            dex(&cpu6502);
+            break;
+        case OP_DEY:
+            dey(&cpu6502);
             break;
         case OP_BRK:
             done = 1;
